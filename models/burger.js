3edbbb37c5a78754a97065f201929070d54d1aea
Rishabh Goel <Rishabh@rishabhs-mbp.lan>
2019-05-09 15:55:24 -0400

    added burger model

---
+++ models/burger.js
@@ -0,0 +1,13 @@
+module.exports = function(sequelize, Sequelize) {
+  var Burger = sequelize.define("Burger", {
+    burger_name: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    devoured: {
+      type: Sequelize.BOOLEAN,
+      defaultValue: false
+    }
+  });
+  return Burger;
+};
